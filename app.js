af5a5ddcb3c2b3afa42b3e947918783464c883dc
Burakcan <burakcanaytac@gmail.com>
2025-05-23 18:47:57 +0300

    feature/new&edit&Delete

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const path = require('path')
 const app = express()
 const Campground = require('./models/campground.js')
 
+app.set('view engne','ejs')
+app.set('views',path.join(__dirname,'views'))
+
+app.use(express.urlencoded({extended: true}))
+
 const db = mongoose.connection;
 mongoose.connect('mongodb://127.0.0.1:27017/Tent-Camp')
 db.on('error', err => console.log(`Connection Failed: ${err}`))
 db.once('open',() => console.log('Database Connected!'))
-
-app.set('view engne','ejs')
-app.set('views',path.join(__dirname,'views'))
-
-// app.use(express.urlencoded({extended: true}))
 
 app.get('/home', (req, res) => {
     res.send('WELCOME TO TENT CAMP!')
@@ -27,14 +27,38 @@ app.get('/campgrounds/new' , async (req, res) => {
     res.render('campgrounds/new.ejs')    
 })
 
-app.get('/campgrounds/edit/:id', async (req,res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit.ejs')
+app.post('/campgrounds', async (req, res) => {
+    await Campground.insertOne(req.body.campground)
+    .then(data => console.log(data))
+    .catch(err => console.log(`Insert Failed ${err}`))
+    res.redirect('./campgrounds')
 })
 
 app.get('/campgrounds/detail/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/detail.ejs', { campground })
+})
+
+app.get('/campgrounds/edit/:id', async (req,res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit.ejs', { campground })
+})
+
+app.post('/campgrounds/edit/:id', async (req, res) => {
+    const { id } = req.params
+    const campground = req.body.campground
+    await Campground.findByIdAndUpdate(id, campground,{new:true})
+        .then(data => console.log(`Success, Updated Data: ${data}`))
+        .catch(err => console.log(`Failed : ${err}`))
+    res.redirect(`/campgrounds/detail/${id}`)
+})
+
+app.post('/campgrounds/delete/:id', async (req,res) => {
+    const { id } = req.params
+    await Campground.findByIdAndDelete(id)
+        .then(data => console.log(`Deleted,${data}`))
+        .catch(err => console.log(`Failed ${err}`))
+    res.redirect('/campgrounds')
 })
 
 app.listen('3000', () => {
